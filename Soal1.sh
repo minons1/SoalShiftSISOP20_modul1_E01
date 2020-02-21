3216c10c4bbdedd83614199faffad3468ee9013b
minons1 <55347970+minons1@users.noreply.github.com>
2020-02-21 17:56:44 +0700

    Update Soal1.sh

--- Soal1.sh
+++ Soal1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 LC_NUMERIC=POSIX awk -F'\t' 'BEGIN {min=100000000};
-{
-if( $21<min && $21 != "") {min=$21;print $21}};
-END {print min}' dataset.tsv
+{if( $21<min && $21 != "") {min=$21;region=$13}}
+END {print min,region}' dataset.tsv
