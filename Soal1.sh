b9ca4e8965e17d98550dc60e71cf8be263d6d867
minons1 <55347970+minons1@users.noreply.github.com>
2020-02-29 22:03:39 +0700

    Update Soal1.sh

--- Soal1.sh
+++ Soal1.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-LC_NUMERIC=POSIX awk -F'\t' 'BEGIN {min=100000000};
-{if( $21<min && $21 != "") {min=$21;region=$13}}
-END {print min,region}' dataset.tsv
+satu=$(awk -F'\t' '{sum[$13]+=$21} END {for (i in sum) print i }' dataset.tsv | sort -g | head -n1)
+echo $satu
+echo " "
+
+dua=$(awk -v satu=$satu -F'\t' '{if ($13==satu) sum[$11]+=$21;} END {for (i in sum) print i }' dataset.tsv | sort -g | head -n2)
+echo $dua
+echo " "
+
+dua=($(echo $dua | tr " " "\n"))
+for i in "${dua[@]}"
+do
+	echo $i
+	awk -v dua=$i -F'\t' 'BEGIN {print dua} {if($11==dua) sum[$17]+=$21;} END {for (i in sum) print i}' dataset.tsv | sort -g | head -n10
+	echo " "
+done
